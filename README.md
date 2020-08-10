d9aa68ae989979391d2081d51b795a93bfbe0089
RezaKarbasi <a.rz.karbasi@gmail.com>
2020-08-10 11:25:55 +0430

    presentation added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # influxDB-tutorial
+
+useful links :
+https://hub.docker.com/_/influxdb
+https://docs.influxdata.com/influxdb/v1.8/introduction/get-started/
+https://influxdb-python.readthedocs.io/en/latest/api-documentation.html
--- producer.py
+++ producer.py
@@ -1,3 +1,25 @@
 from influxdb import InfluxDBClient
+import datetime
+import numpy as numpy
 
-client = InfluxDBClient(database='mydb')
+from influxdb import InfluxDBClient
+import datetime
+import numpy as np
+import time
+
+client = InfluxDBClient(database='shop') # default port 8086
+while True:
+    dt = str(datetime.datetime.now())
+    room = np.random.randint(1,5)
+    price = int(np.random.random()*50+10)/10
+    weight = int(price/20 + np.random.random()*3)/10
+
+    json_body = [{
+                    'measurement':'customer',
+                    'tags':{'room':room},
+                    'fields':{'price':price,'weight':weight}#,
+                    # 'time':dt
+                }]
+    client.write_points(json_body)
+    print(json_body[0])
+    time.sleep(0.1)
